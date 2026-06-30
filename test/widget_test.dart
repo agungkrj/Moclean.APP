761b0bfee84d97eb321d6636501df1f444a84194
agungkrj <agungkrj2@gmail.com>
2026-06-30 14:27:07 +0700

    Upload project MoClean

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,21 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-<<<<<<< HEAD
-import 'package:moclienapp/main.dart';
-=======
-import 'package:mocliean/main.dart';
->>>>>>> 36f1c013247f0425a92a148b9ef912be90b92e82
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-<<<<<<< HEAD
-    await tester.pumpWidget(MyApp());
-=======
-    await tester.pumpWidget(const MyApp());
->>>>>>> 36f1c013247f0425a92a148b9ef912be90b92e82
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
